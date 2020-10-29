4ea926d1ce837623b5b453de921376903d0c4933
var787 <hegde.rakshan@gmail.com>
2020-10-28 20:52:37 -0500

    commit tree adb8b34b6e7958336586186b2bc934fbb7db1034

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
+    new_arr=self.split(/[!.?]+/) #Why the one and more operator?
+    new_arr.length
+    
+  
+
 
   end
 end
